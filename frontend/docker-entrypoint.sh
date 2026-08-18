16e0088370c1c438a5511814d06274217c50ad3a
Ashrith <ashrith.js@henshingo.com>
2026-08-18 20:50:00 +0530

    Frontend nginx: use BACKEND_HOST for SNI, parse from BACKEND_URL

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,6 +3,11 @@ set -e
 
 : "${BACKEND_URL:?BACKEND_URL must be set, e.g. https://corporate-diplomat-backend.onrender.com}"
 
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+BACKEND_HOST=$(echo "$BACKEND_URL" | sed -E 's#^https?://##; s#/.*$##')
+export BACKEND_HOST
+
+envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "==> proxy_pass ${BACKEND_URL} (host: ${BACKEND_HOST})"
 
 exec nginx -g 'daemon off;'
